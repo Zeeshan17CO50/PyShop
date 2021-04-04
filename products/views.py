b5266edc0e5b0cc33d5c9ceb789d79c52b7ea6ba
Zeeshan17CO50 <mohammedsiddiquezee@gmail.com>
2021-04-04 12:13:46 +0530

    PyShop Initial commits

---
+++ products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def front(request):
+    return render(request, 'front.html')
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+
+def new(request):
+    return HttpResponse('New Products')
